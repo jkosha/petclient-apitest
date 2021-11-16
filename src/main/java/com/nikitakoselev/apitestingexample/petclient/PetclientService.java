a4e7c9c643ae67da628eeb2fd6c3cadc75b3e99a
jkosha <nikita.koselev@gmail.com>
2021-11-16 16:13:38 +0000

    initial commit (empty project)

---
+++ src/main/java/com/nikitakoselev/apitestingexample/petclient/PetclientService.java
@@ -0,0 +1,13 @@
+package com.nikitakoselev.apitestingexample.petclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclientService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclientService.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikitakoselev/apitestingexample/petclient/PetclientServiceTests.java
@@ -0,0 +1,13 @@
+package com.nikitakoselev.apitestingexample.petclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclientServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
